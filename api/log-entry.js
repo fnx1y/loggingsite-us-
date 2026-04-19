1fce7232f93a5d4f734590ee1c442765128180fe
fnx1y <zosuoyt@gmail.com>
2026-04-19 14:49:51 -0700

    Update log-entry.js

--- api/log-entry.js
+++ api/log-entry.js
@@ -21,9 +21,7 @@ export default async function handler(req, res) {
       }
     }
 
-    return res.status(200).json({
-      ok: true
-    });
+    return res.status(200).json({ ok: true });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error." });
   }
